6be400c0b6b2c62f93eddf7718539d953c78f757
junjeong <wn8624@naver.com>
2025-05-31 23:08:14 +0900

    fix: Interview 부분 강조처리 추가

--- src/components/CommentBox.tsx
+++ src/components/CommentBox.tsx
@@ -1,13 +1,18 @@
+import { ReactNode } from "react";
+
 interface CommentBoxProps {
   question: string;
-  answer: string;
+  answer: ReactNode;
 }
 
 const CommentBox = ({ question, answer }: CommentBoxProps) => {
   return (
     <div className="bg-[#373737] rounded-l-2xl rounded-tr-2xl w-full h-[176px] p-5 text-lg">
       <h4 className="text-2xl">{question}</h4>
-      <p className="mt-4 text-base">{answer}</p>
+      <p
+        className="mt-4 text-base"
+        dangerouslySetInnerHTML={{ __html: answer }}
+      />
     </div>
   );
 };
